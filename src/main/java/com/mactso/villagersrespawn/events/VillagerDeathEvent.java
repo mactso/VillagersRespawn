073e963140d9181b6fb0970245463536a4e24392
mactso <stephenmcleod2004@yahoo.com>
2023-04-19 22:20:53 -0500

    Vers 7 for 1.19.3

--- src/main/java/com/mactso/villagersrespawn/events/VillagerDeathEvent.java
+++ src/main/java/com/mactso/villagersrespawn/events/VillagerDeathEvent.java
@@ -2,14 +2,20 @@ package com.mactso.villagersrespawn.events;
 
 import java.util.Optional;
 
+import org.joml.Vector3d;
+
 import com.mactso.villagersrespawn.config.MyConfig;
 
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.level.block.BedBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.GlobalPos;
 import net.minecraft.world.Difficulty;
 import net.minecraft.server.level.ServerLevel;
@@ -17,23 +23,24 @@ import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class VillagerDeathEvent {
-	private static final int[] xpLevels = new int[]{0, 10, 70, 150, 250};
+	private static final int[] xpLevels = new int[] { 0, 10, 70, 150, 250 };
+
 	@SubscribeEvent
-    public void doVillagerRespawn(LivingDeathEvent event) { 
-		
+	public void doVillagerRespawn(LivingDeathEvent event) {
+
 		Entity eventEntity = event.getEntity();
 		if (event.getEntity() == null) {
 			return;
 		}
-		
+
 		if (!(eventEntity.level instanceof ServerLevel)) {
 			return;
 		}
-		
+
 		if (eventEntity instanceof Villager) {
 			// chance villager will really die
 			double randomD100Roll = eventEntity.level.random.nextDouble() * 100;
-			randomD100Roll = Math.ceil(randomD100Roll); 
+			randomD100Roll = Math.ceil(randomD100Roll);
 			Difficulty difficulty = eventEntity.level.getDifficulty();
 			if (difficulty == Difficulty.NORMAL) {
 				randomD100Roll += 5.0;
@@ -46,49 +53,45 @@ public class VillagerDeathEvent {
 			}
 
 			Villager ve = (Villager) eventEntity;
-			
+
 			// if zombie deaths true, will still become zombies in HARD mode
-			String deathMessage="Died for unknown reasons.";
-			int deathX, deathY, deathZ;
+
 			if (ve.getLastDamageSource() != null) {
-				deathMessage = ve.getLastDamageSource().getLocalizedDeathMessage(ve).toString();
 				if (ve.getLastDamageSource().getEntity() instanceof Zombie) {
-					if ((ve.level.getDifficulty()== Difficulty.HARD ) && (MyConfig.hardModeZombieDeaths))
-					return;
+					if ((ve.level.getDifficulty() == Difficulty.HARD) && (MyConfig.hardModeZombieDeaths))
+						return;
 				}
 			}
-			//villager will respawn if they have a bed
-			Brain<Villager> vb =  ve.getBrain();
+			// villager will respawn if they have a bed
+			Brain<Villager> vb = ve.getBrain();
 			Optional<GlobalPos> villagerHome = vb.getMemory(MemoryModuleType.HOME);
 			if (villagerHome.isPresent()) {
 				GlobalPos gVHP = villagerHome.get();
 				BlockPos villagerHomePos = gVHP.pos();
-				deathX = (int) ve.getX(); deathY = (int) ve.getY(); deathZ = (int) ve.getZ();
-				eventEntity.setPos(villagerHomePos.getX(), villagerHomePos.getY(), villagerHomePos.getZ() );
-				ve.clearFire();
-				ve.setHealth(MyConfig.respawnHealth);
+				BlockState bs = ve.getLevel().getBlockState(villagerHomePos);
+				if (bs.getBlock() instanceof BedBlock) {
+					Optional<Vec3> standupPos = BedBlock.findStandUpPosition(ve.getType(), ve.getLevel(),
+							villagerHomePos, bs.getValue(BedBlock.FACING) , 0);
 
-				if (MyConfig.respawnXpLoss) {
-
-					int level = ve.getVillagerData().getLevel();
-					ve.overrideXp(xpLevels[level-1]);
-
+					if (standupPos.isPresent()) {
+						eventEntity.setPos(standupPos.get().x, standupPos.get().y, standupPos.get().z);
+						ve.clearFire();
+						ve.removeAllEffects();
+						ve.setHealth(MyConfig.respawnHealth);
+						doRespawnXpLoss(ve);
+						event.setCanceled(true);
+					}
 				}
-				
-				if (MyConfig.debugLevel > 0) {
-					
-					System.out.println("VillagersRespawn: Villager " 
-								+ deathMessage );
-					System.out.println(  
-							" at " + deathX +", " + deathY +", " + deathZ + ".");
+			}
+		}
+	}
 
-					System.out.println(
-							" Respawned at " + ve.getX() +", " + ve.getY() +", " + ve.getZ() + ".");
-
-				}
-				event.setCanceled(true);
+	private void doRespawnXpLoss(Villager ve) {
+		if (MyConfig.respawnXpLoss) {
+			int level = ve.getVillagerData().getLevel();
+			if (level >= 1) {
+				ve.overrideXp(xpLevels[level - 1]);
 			}
 		}
 	}
 }
- 
